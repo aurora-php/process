1befaf8c489a0cab104c8d2ec828b87898557fc1
Harald Lapp <harald@octris.org>
2015-07-04 09:02:45 +0200

    changes, use composer generated autoloader

--- example/example.php
+++ example/example.php
@@ -1,14 +1,9 @@
 #!/usr/bin/env php
 <?php
 
-require_once('../libs/Process.php');
-require_once('../libs/Child.php');
-require_once('../libs/Messaging.php');
-require_once('../libs/ProcessController.php');
-require_once('../libs/ProcessException.php');
-require_once('../libs/SocketException.php');
+require_once('../vendor/autoload.php');
 
-class worker extends \Octris\Proc\Child {
+class worker extends \Octris\Process\Child {
     function run() {
         while (true) {
             if (($msg = $this->messaging->recv()) !== false) {
@@ -18,26 +13,20 @@ class worker extends \Octris\Proc\Child {
     }
 }
 
-class main extends \Octris\Proc\Process {
-    function run() {
-        $child = $this->fork('worker');
+$proc = new \Octris\Process();
+$child = $proc->fork('worker');
 
-        $cnt = 1;
+$cnt = 1;
 
-        while ($cnt < 4) {
-            $child->send('Test #' . $cnt);
+while ($cnt < 4) {
+    $child->send('Test #' . $cnt);
 
-            do {
-                if (($msg = $child->recv()) !== false) {
-                    print trim($msg) . "\n";
-                    break;
-                }
-            } while(true);
-
-            ++$cnt;
+    do {
+        if (($msg = $child->recv()) !== false) {
+            print trim($msg) . "\n";
+            break;
         }
-    }
-}
+    } while(true);
 
-$main = new main();
-$main->run();
+    ++$cnt;
+}
