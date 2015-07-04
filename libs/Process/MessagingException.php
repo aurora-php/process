f576b6d94540ef62298d6aaf0e5e9b297c18b0cc
Harald Lapp <harald@octris.org>
2015-07-04 23:13:52 +0200

    initial commit new exception for messaging errors

---
+++ libs/Process/MessagingException.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the 'octris/proc' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Octris\Process;
+
+/**
+ * Socket exception class.
+ *
+ * @copyright   copyright (c) 2015 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+class MessagingException extends \Exception
+{
+}
