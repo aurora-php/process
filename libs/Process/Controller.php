0b798905ea4922bba49efbc97c89f16f34806323
Harald Lapp <h.lapp@clipdealer.de>
2015-07-06 13:06:12 +0200

    added more functionality ...
    - to check if a process is still alive
    - to send signals to the process
    - to return the process ID

--- libs/Process/Controller.php
+++ libs/Process/Controller.php
@@ -27,11 +27,55 @@ class Controller
     protected $messaging;
 
     /**
-     * Constructor.
+     * Process ID to be controlled.
+     *
+     * @type    int
      */
-    public function __construct(\Octris\Process\Messaging $messaging)
+    protected $pid;
+
+    /**
+     * Constructor.
+     * 
+     * @param   \Octris\Process\Messaging   $messaging  Instance of messaging service.
+     * @param   int                         $pid        Process ID the controller belongs to.
+     */
+    public function __construct(\Octris\Process\Messaging $messaging, $pid)
     {
         $this->messaging = $messaging;
+        $this->pid = $pid;
+    }
+
+    /**
+     * Test if the process is alive.
+     * 
+     * @param   bool                                    Returns true if the child process is still running.
+     */
+    public function isAlive()
+    {
+        $result = pcntl_waitpid($this->pid, $status, WNOHANG);
+        
+        return ($result === 0);
+    }
+    
+    /**
+     * Return process ID.
+     * 
+     * @return  int                                     Process ID.
+     */
+    public function getPid()
+    {
+        return $this->pid;
+    }
+    
+    /**
+     * Send a signal to the child process. The signal can be one of those listed at:
+     * http://php.net/manual/de/pcntl.constants.php.
+     * 
+     * @param   int                         $signal     Signal to send.
+     */
+    public function kill($signal)
+    {
+        posix_kill($this->pid, $signal);
     }
 
     /**
